678b89fe3f19049646e38211b3b7bf2542a25ec0
Andy Little <Andy Little>
2016-01-14 14:51:36 +0000

    streamline command line args in main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 
-
+#include <cstring>
 #include "config.hpp"
 #include "platform.hpp"
 #include "dependency_list.hpp"
-#include <cstring>
+
 #if defined (AIR_INSTALLER_PLATFORM_WINDOWS)
 #include "Windows.h"
 #endif
@@ -12,8 +12,8 @@
    msys utilities to test for
    
    msys-wget
-   mysy-tar ( Linux only)
-   msys-bzip2 ( linux Only)
+   #mysy-tar ( not used on windows )
+   #msys-bzip2 (not used on windows)
    msys-unzip
    unzip
 */
@@ -26,34 +26,75 @@ namespace {
    {
       cleanup_platform();
    }
-}
 
-// useage progname [ --stage dir ] [--install dir ]
+   enum arg_id{
+	  UNKNOWN_ARG
+	  ,STAGE_DIR
+	  ,BIN_DIR
+	  ,LIB_DIR
+   };
+
+   int arg_to_id(const char* arg)
+   {
+	   if ( strcmp(arg,"--stage-dir") == 0 ){
+		   return STAGE_DIR;
+	   }
+	   if ( strcmp(arg,"--bin-dir") == 0 ){
+	   	   return BIN_DIR;
+	   }
+	   if ( strcmp(arg,"--lib-dir") == 0 ){
+	  	   return LIB_DIR;
+	   }
+	   return UNKNOWN_ARG;
+   }
+
+// useage:
+//progname [ --stage-dir dir ] [--lib-dir dir ] [--bin-dir dir]
 
 bool parse_args(int argc , const char* argv[])
 {
-   int argnum = 1;
+   int argnum = 1; // ignore progname
    while (argnum < argc) {
-
-      if ( strcmp(argv[argnum],"--stage") == 0 ){
+      int argid = arg_to_id(argv[argnum]);
+	  switch(argid){
+	  case STAGE_DIR:
+	  case BIN_DIR:
+	  case LIB_DIR:
          if ( argc > (argnum + 1) ){
-            std::string stage_dir = argv[argnum + 1];
-            if ( get_platform()->set_temp_dir(stage_dir)){
-               argnum += 2;
-            }else{
-               return false;
+            std::string target_dir = argv[argnum + 1];
+            std::cout << "target dir = " << target_dir  <<'\n';
+            bool result = false;
+            switch (argid){
+            case STAGE_DIR:
+            	result = get_platform()->set_temp_dir(target_dir);
+            	break;
+      	    case BIN_DIR:
+      	    	result = get_platform()->set_bin_dir(target_dir);
+      	    	break;
+      	    case LIB_DIR:
+      	    	result = get_platform()->set_lib_dir(target_dir);
+      	    	break;
             }
-            
+            if ( result ){
+               argnum += 2;
+               break;
+            }else{
+               return false; // has reported
+            }
          }else{
             std::cout << "expected temp dir name\n";
             return false;
          }
-      }else{
-         std::cout << "unknown arg\n";
-      }
+
+	  default:
+		  std::cout << "unknown arg\n";
+		  return false;
+	  }
    }
    return true;
 }
+
+}// namespace
 
 int main(int argc , const char* argv[])
 {
