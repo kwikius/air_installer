05d1ff6b81d50e24006fcdb0e1b27a396ec46833
Andy Little <Andy Little>
2016-01-14 14:47:20 +0000

    fill in file_utils for windows

--- file_utils.cpp
+++ file_utils.cpp
@@ -3,7 +3,6 @@
 #include <stdexcept>
 
 #if defined(AIR_INSTALLER_PLATFORM_UNIX)
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -18,7 +17,7 @@ bool file_exists( std::string const&  filename)
   struct stat sb;
   return( (stat(filename.c_str(), &sb) == 0) && S_ISREG(sb.st_mode));
 #elif defined(AIR_INSTALLER_PLATFORM_WINDOWS)
-  DWORD flags GetFileAttributes(filename.c_str());
+  DWORD flags = GetFileAttributes(filename.c_str());
   return ( (flags != INVALID_FILE_ATTRIBUTES) && !(flags & FILE_ATTRIBUTE_DIRECTORY));
 #else
   #error "need def for this platform"
@@ -31,7 +30,7 @@ bool dir_exists( std::string const& dirname)
   struct stat sb;
   return( (stat(dirname.c_str(), &sb) == 0) && S_ISDIR(sb.st_mode));
 #elif defined(AIR_INSTALLER_PLATFORM_WINDOWS)
-  DWORD flags GetFileAttributes(dirname.c_str());
+  DWORD flags = GetFileAttributes(dirname.c_str());
   return ( (flags != INVALID_FILE_ATTRIBUTES) && (flags & FILE_ATTRIBUTE_DIRECTORY));
 #else
   #error "need def for this platform"
@@ -47,18 +46,30 @@ std::string get_working_dir()
    }
    return buf;
 #elif defined(AIR_INSTALLER_PLATFORM_WINDOWS)
-#error "todo"
+  char buf[MAX_PATH];
+  DWORD result = GetCurrentDirectory(MAX_PATH,buf);
+  if(result == 0 ){
+	  throw std::runtime_error("pwd failed");
+  }
+  if (result > MAX_PATH ){
+	  throw std::runtime_error("pwd failed path too long");
+  }
+  return buf;
+#else
+  #error "need def for this platform"
 #endif
 }
 void change_wkg_dir_to(std::string const & dir)
 {
 #if defined(AIR_INSTALLER_PLATFORM_UNIX)
    if ( chdir(dir.c_str()) != 0){
+#elif defined(AIR_INSTALLER_PLATFORM_WINDOWS)
+   if (!SetCurrentDirectory(dir.c_str())){
+#else
+  #error "need def for this platform"
+#endif
       throw std::runtime_error("cd failed");
    }
-#elif defined(AIR_INSTALLER_PLATFORM_WINDOWS)
-#error "todo"
-#endif
 }
 
 std::string get_zoomworks_deps_dir() 
