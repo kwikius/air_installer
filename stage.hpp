2a274c3102f4c7e0a7346f4e673e70ad1501d3b2
skyscraper <kwikius@yahoo.com>
2016-01-15 18:14:24 +0000

    add set_default dir fun to stage

--- stage.hpp
+++ stage.hpp
@@ -9,6 +9,8 @@ struct stage_t{
     ~stage_t();
      std::string const & get_temp_dir_name() const { return m_temp_dir_name;}
      bool set_dir( std::string const & dir);
+
+     bool set_default_dir();
     private:
      std::string m_temp_dir_name;
      bool m_remove_dir_on_exit;
