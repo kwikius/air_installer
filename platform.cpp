2e9606aa2841fbf4ba74709ac62f5ba11da90cb1
skyscraper <kwikius@yahoo.com>
2016-01-15 18:16:12 +0000

    remove creation of default lib and bin dirs from platform ctor

--- platform.cpp
+++ platform.cpp
@@ -68,72 +68,82 @@
       return true;
    }
    void platform_t::set_defaults()
-   { // for testing
-     const char* subdirs[] = {"lib_dummy","stuff"};
-#if defined(AIR_INSTALLER_PLATFORM_UNIX)
-     // set lib dir to $HOME 
-     const char* str_home = getenv("HOME");
-#elif defined (AIR_INSTALLER_PLATFORM_WINDOWS)
-     const char*  str_home = getenv("USERPROFILE");
-#else
-#error "shouldnt get here"
-#endif
-     if ( str_home){
-        std::string lib_dir = str_home;
-        	        
-     #if defined(AIR_INSTALLER_PLATFORM_UNIX)   
-        struct stat sb;
-        if ( (stat(lib_dir.c_str(), &sb) == 0) && S_ISDIR(sb.st_mode) ){
-            for ( uint32_t i = 0; i < (sizeof(subdirs) / sizeof(subdirs[0])); ++i){
-               // no dir sep applied on linux
-               if ( lib_dir.at( lib_dir.length() -1) != get_platform()->get_dir_sep().at(0)){
-                   lib_dir += get_platform()->get_dir_sep().at(0);
-               }
-               lib_dir += subdirs[i];
-               // continue making dirs
-                 if ( stat(lib_dir.c_str(), &sb) == 0){
+   { 
+     m_stage.set_default_dir();
+    if (( m_lib_dir == "") || ( m_bin_dir == "")){
+        // for testing
+        const char* subdirs[] = {"lib_dummy","stuff"};
+   #if defined(AIR_INSTALLER_PLATFORM_UNIX)
+        // set lib dir to $HOME 
+        const char* str_home = getenv("HOME");
+   #elif defined (AIR_INSTALLER_PLATFORM_WINDOWS)
+        const char*  str_home = getenv("USERPROFILE");
+   #else
+   #error "shouldnt get here"
+   #endif
+        if ( str_home){
+           std::string lib_dir = str_home;   
+        #if defined(AIR_INSTALLER_PLATFORM_UNIX)   
+           struct stat sb;
+           if ( (stat(lib_dir.c_str(), &sb) == 0) && S_ISDIR(sb.st_mode) ){
+               for ( uint32_t i = 0; i < (sizeof(subdirs) / sizeof(subdirs[0])); ++i){
+                  // no dir sep applied on linux
+                  if ( lib_dir.at( lib_dir.length() -1) != get_platform()->get_dir_sep().at(0)){
+                      lib_dir += get_platform()->get_dir_sep().at(0);
+                  }
+                  lib_dir += subdirs[i];
+                  // continue making dirs
+                    if ( stat(lib_dir.c_str(), &sb) == 0){
                         if (S_ISDIR(sb.st_mode)){
                            continue;
                         }else{
                            throw std::runtime_error("def home lib failed"); 
                         }
+                    }else{
+                       std::string cmd = "mkdir " + lib_dir;
+                       system(cmd.c_str());
+                    }
+               }
+               if ( m_lib_dir == "") {
+                   this->set_lib_dir(lib_dir);
+               }
+               if ( m_bin_dir == ""){
+                  this->set_bin_dir(lib_dir);
+               }
+            }
+         #else
+             DWORD flags = GetFileAttributes(lib_dir.c_str());
+             if ( (flags != INVALID_FILE_ATTRIBUTES) && ( flags & FILE_ATTRIBUTE_DIRECTORY) ){
+                 for ( uint32_t i = 0; i < (sizeof(subdirs) / sizeof(subdirs[0])); ++i){
+                 if ( lib_dir.at( lib_dir.length() -1) != get_platform()->get_dir_sep().at(0)){
+                    lib_dir += get_platform()->get_dir_sep().at(0);
+                 }
+                 lib_dir += subdirs[i];
+                 flags = GetFileAttributes(lib_dir.c_str());
+                 if ((flags != INVALID_FILE_ATTRIBUTES) && ( flags & FILE_ATTRIBUTE_DIRECTORY) ){
+                   // subdir exists already
+                   continue;
                  }else{
-                    std::string cmd = "mkdir " + lib_dir;
-                    system(cmd.c_str());
+                    if ( !CreateDirectory(lib_dir.c_str(), NULL) ){
+                       throw std::runtime_error("faied to create def bin and lib subdirs");
+                    }
                  }
-            }
-            this->set_lib_dir(lib_dir);
-            this->set_bin_dir(lib_dir);
-         }
-      #else
-          DWORD flags = GetFileAttributes(lib_dir.c_str());
-          if ( (flags != INVALID_FILE_ATTRIBUTES) && ( flags & FILE_ATTRIBUTE_DIRECTORY) ){
-              for ( uint32_t i = 0; i < (sizeof(subdirs) / sizeof(subdirs[0])); ++i){
-              if ( lib_dir.at( lib_dir.length() -1) != get_platform()->get_dir_sep().at(0)){
-                 lib_dir += get_platform()->get_dir_sep().at(0);
-              }
-              lib_dir += subdirs[i];
-              flags = GetFileAttributes(lib_dir.c_str());
-              if ((flags != INVALID_FILE_ATTRIBUTES) && ( flags & FILE_ATTRIBUTE_DIRECTORY) ){
-                // subdir exists already
-                continue;
-              }else{
-                 if ( !CreateDirectory(lib_dir.c_str(), NULL) ){
-                    throw std::runtime_error("faied to create def bin and lib subdirs");
-                 }
-              }
-            }
-            this->set_lib_dir(lib_dir);
-            this->set_bin_dir(lib_dir);
-          }else{
-            throw std::runtime_error("faied to create def bin and lib dir");
-          }
-      #endif
-         
-     }else{
-         throw std::runtime_error("couldnt get home dir");
+               }
+               if ( m_lib_dir == "") {
+                   this->set_lib_dir(lib_dir);
+               }
+               if ( m_bin_dir == ""){
+                  this->set_bin_dir(lib_dir);
+               }
+             }else{
+               throw std::runtime_error("faied to create def bin and lib dir");
+             }
+         #endif
+            
+        }else{
+            throw std::runtime_error("couldnt get home dir");
+        }
      }
-
    }
 namespace {
    platform_t*  platform = nullptr;
@@ -143,7 +153,6 @@ platform_t* get_platform()
 {
    if ( platform == nullptr){
      platform = new platform_t;  
-     platform->set_defaults();   
    }
    return platform;
 }
