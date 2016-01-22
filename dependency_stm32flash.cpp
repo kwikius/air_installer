68fd5377c1f1782b1c54439b6a06710cb9f79ea4
skyscraper <kwikius@yahoo.com>
2016-01-22 12:38:28 +0000

    initial commit of stm32flash install

---
+++ dependency_stm32flash.cpp
@@ -0,0 +1,34 @@
+
+#include<cstdlib>
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+#include <cassert>
+
+#include "config.hpp"
+#include "simple_dependency.hpp"
+#include "stage.hpp"
+#include "platform.hpp"
+#include "file_utils.hpp"
+
+/*
+   download is same
+   stage is same
+   install 
+*/
+
+struct dependency_stm32flash : dependency_t{
+// url = "http://sourceforge.net/projects/stm32flash/files/stm32flash-0.4.tar.gz"
+  dependency_stm32flash
+ :dependency_t{dependency_t::STM32FLASH}{}
+
+   bool install() override
+   {
+      // look for existence in bin dir
+      // look for existence in global path
+      
+      // change to temp dir
+      // make a subdir stm32flash
+   }
+
+};
